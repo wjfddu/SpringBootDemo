2b4419fac84b18519e90b7bf7f575874851f043a
wjf <421818946@qq.com>
2023-08-31 12:04:37 +0800

    1.logback日志实践

--- common-web/src/main/java/com/wjf/demo/commonweb/config/GlobalException.java
+++ common-web/src/main/java/com/wjf/demo/commonweb/config/GlobalException.java
@@ -1,6 +1,8 @@
 package com.wjf.demo.commonweb.config;
 
 import com.wjf.demo.commonweb.vo.ResultVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalException {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalException.class);
+
     @ExceptionHandler(Exception.class)
     public ResultVO handleException(Exception e){
         String message = e.getCause() == null ? e.getMessage() : e.getCause().getMessage();
+        logger.error("全局异常捕获：{}", e);
         return ResultVO.error(message);
     }
 
